9eb6be23acb65afaac0786566a95aa598f7637cd
Kevin 張 <kevin30302456@gmail.com>
2022-12-10 17:12:15 +0800

    Add PlayerMovement,Camera,Rope script.

---
+++ Assets/Script/Player/Movement/CameraControl.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    [SerializeField] float mouseSensitivity = 100f;
+    [SerializeField] Transform playerBody;
+    float xRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); // 讓頭部旋轉在90度
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Script/Player/Movement/GrapplingGun.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrapplingGun : MonoBehaviour
+{
+    private LineRenderer lr;
+    private Vector3 grapplePoint;
+    public LayerMask whatIsGrappleable;
+    public Transform gunTip, player;
+    public new Transform camera;
+    private float maxDistance = 100f;
+    private SpringJoint joint;
+
+    private void Awake()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+    private void Update()
+    {
+        DrawRope();
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            StartGrapple();
+        }
+        else if (Input.GetKeyUp(KeyCode.Q))
+        {
+            StopGrapple();
+        }
+    }
+    //Call whenever we want to start a grapple
+    private void StartGrapple()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(camera.position, camera.forward, out hit, maxDistance))
+        {
+            grapplePoint = hit.point;
+            joint = player.gameObject.AddComponent<SpringJoint>();
+            joint.autoConfigureConnectedAnchor = false;
+            joint.connectedAnchor = grapplePoint;
+
+            float distanceFromPoint = Vector3.Distance(player.position, grapplePoint);
+            //The distance grapple will try to keep from grapple point.
+            joint.maxDistance = distanceFromPoint * 0.7f;
+            joint.minDistance = distanceFromPoint * 0.25f;
+            //Change these value to fit your game.
+            joint.spring = 20f;
+            joint.damper = 6f;
+            joint.massScale = 3f;
+
+            lr.positionCount = 2;
+        }
+    }
+
+    void DrawRope()
+    {
+        if (!joint) return;//If not grappling, don't draw rope
+        else
+        {
+            lr.SetPosition(0, gunTip.position);
+            lr.SetPosition(1, grapplePoint);
+        }
+    }
+    private void StopGrapple()
+    {
+        lr.positionCount = 0;
+        Destroy(joint);
+    }
+    public bool IsGrappling()
+    {
+        return joint != null;
+    }
+    public Vector3 GetGrapplePoint()
+    {
+        return grapplePoint;
+    }
+}
