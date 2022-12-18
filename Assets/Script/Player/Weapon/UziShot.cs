e72e122be3bcabee023f956d6e7050b2f4b94dd9
Kevin 張 <kevin30302456@gmail.com>
2022-12-18 17:53:12 +0800

    Add Uzi Animation Script.

--- Assets/Script/Player/Weapon/UziShot.cs
+++ Assets/Script/Player/Weapon/UziShot.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class UziShot : MonoBehaviour
 {
+    Animator animator;
+    AnimatorStateInfo info;
+    int BulletNumber = 60;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        info = animator.GetCurrentAnimatorStateInfo(0);
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+            animator.SetBool("isShotting", true);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            animator.SetBool("isShotting", false);
+        }
+    }
+    void Shot()
+    {
+
     }
 }
