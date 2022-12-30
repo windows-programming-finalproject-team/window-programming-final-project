faf1a9e1697e6f73040564a1d1c3a5cff76f4aed
YUANWRLD <chang920327@gmail.com>
2022-12-30 13:11:33 +0800

    UI transition done

---
+++ Assets/Script/UI/PauseController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseController : MonoBehaviour
+{
+    [SerializeField] GameObject target;
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            PauseGame();
+        }
+    }
+    void PauseGame()
+    {
+        target.SetActive(true);
+        Time.timeScale = 0;
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+    }
+}
