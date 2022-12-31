44c4c45be1bd20aad10a853ee40621a43a33a80e
ascodeasice <ab0988956087@gmail.com>
2022-12-31 17:06:13 +0800

    Add muzzle flash and reload to UZI

--- Assets/Script/Player/Weapon/UziShot.cs
+++ Assets/Script/Player/Weapon/UziShot.cs
@@ -9,32 +9,63 @@ public class UziShot : MonoBehaviour
     [SerializeField] GameObject Bullet;
     Rigidbody rb;
     [SerializeField] int BulletNumber = 60;
+    [SerializeField] ParticleSystem muzzleFlash;
+    int maxBulletNumber = 60;
+    float reloadTime = 0.9f;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        muzzleFlash.Stop();
+    }
+
+    // need to use this function for switching weapon
+    private void OnEnable()
+    {
+        muzzleFlash.Stop();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log($"UZI ammo:{BulletNumber}");
+        // NOTE add else to prevent reloading while shooting
+
+        // start shooting
         if (Input.GetMouseButtonDown(0) && BulletNumber > 0)
         {
+            // lmao, what is "isShotting"
             animator.SetBool("isShotting", true);
+            muzzleFlash.Play();
         }
-        if (BulletNumber <= 0 || Input.GetMouseButtonUp(0))
+        // end shooting when you stopped/ you can't
+        else if (BulletNumber <= 0 || Input.GetMouseButtonUp(0))
         {
             animator.SetBool("isShotting", false);
+            muzzleFlash.Stop();
         }
+
+        // start reloading
         if (Input.GetKeyDown(KeyCode.R) || BulletNumber == 0)
         {
-            if(BulletNumber < 60)
+            if(BulletNumber < maxBulletNumber)
             {
                 transform.parent.parent.parent.GetComponent<SwitchWeapon>().isReloading = true;
                 animator.SetBool("isReloading", true);
+                muzzleFlash.Stop();
+                
+                // force player to wait certain time so I don't need to detect animation
+                StartCoroutine(waitForReload());
             }
         }
     }
+
+    IEnumerator waitForReload()
+    {
+        yield return new WaitForSecondsRealtime(reloadTime);
+        DoneReloading();
+    }
+
     void Shot()
     {
         if (BulletNumber > 0)
@@ -45,9 +76,9 @@ public class UziShot : MonoBehaviour
             rb.velocity = 50 * UziBullet.transform.forward;
         }
     }
-    void Reload()
+    void DoneReloading()
     {
-        BulletNumber = 60;
+        BulletNumber = maxBulletNumber;
         animator.SetBool("isReloading", false);
         transform.parent.parent.parent.GetComponent<SwitchWeapon>().isReloading = false;
     }
