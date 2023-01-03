c3c4875629909abc6f72633cf2a91253746417e4
ascodeasice <ab0988956087@gmail.com>
2023-01-03 16:20:37 +0800

    Fix bug of unable to jump around shotgun tile

--- Assets/Script/Player/Movement/GroundCheck.cs
+++ Assets/Script/Player/Movement/GroundCheck.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class GroundCheck : MonoBehaviour
 {
     [SerializeField] LayerMask ground;
+    [SerializeField] LayerMask shotgunTile;
     public bool isOnGround = true;
 
     private void Update()
     {
-        isOnGround = Physics.CheckSphere(transform.position, .2f, ground);
+        isOnGround = Physics.CheckSphere(transform.position, .2f, ground)
+            || Physics.CheckSphere(transform.position, .2f, shotgunTile);
     }
 }
